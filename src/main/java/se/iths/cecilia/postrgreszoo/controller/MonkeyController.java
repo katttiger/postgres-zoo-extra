1b2e56dd08f710b9d8750d48fe434d8b08d249be
katttiger <katttiger@gmail.com>
2026-02-10 15:38:03 +0100

    Added page for monkeys.

--- src/main/java/se/iths/cecilia/postrgreszoo/controller/MonkeyController.java
+++ src/main/java/se/iths/cecilia/postrgreszoo/controller/MonkeyController.java
@@ -2,8 +2,8 @@ package se.iths.cecilia.postrgreszoo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import se.iths.cecilia.postrgreszoo.model.Monkey;
 import se.iths.cecilia.postrgreszoo.repository.MonkeyRepository;
 import se.iths.cecilia.postrgreszoo.service.MonkeyService;
 
@@ -23,6 +23,47 @@ public class MonkeyController {
     public String getAllMonkeys(Model model) {
         model.addAttribute("monkeys", monkeyService.getAllMonkeys());
         return "monkeys";
+    }
 
+    @GetMapping("/{id}")
+    public String getMonkeyById(@PathVariable Long id, Model model) {
+        model.addAttribute("monkey", monkeyService.getMonkey(id));
+        return "monkey";
+    }
+
+    @PostMapping("/create")
+    public String saveMonkey(@ModelAttribute Monkey monkey) {
+        monkeyService.createMonkey(monkey);
+        return "redirect:/monkeys";
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        return "createMonkey";
+    }
+
+    @PostMapping
+    public String createMonkey(@ModelAttribute Monkey monkey) {
+        monkeyService.createMonkey(monkey);
+        return "redirect:/monkeys";
+    }
+
+    @PutMapping("/{id}")
+    public String updateMonkey(@PathVariable Long id, @ModelAttribute Monkey monkey) {
+        monkeyService.updateMonkey(id, monkey);
+        return "redirect:/monkeys";
+    }
+
+    @GetMapping("/{id}/update")
+    public String showUpdateForm(@PathVariable Long id, Model model) {
+        Monkey monkey = monkeyService.getMonkey(id);
+        model.addAttribute("monkey", monkey);
+        return "redirect:/monkeys";
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public String deleteMonkeyById(@PathVariable Long id) {
+        monkeyService.deleteMonkey(id);
+        return "redirect:/monkeys";
     }
 }
